4538f38dd0ddf4ab62c2f8228197c3a6fb8fd8bb
Smialkowski, Tobias <tobias.smialkowski@comline.de>
2021-04-14 15:13:06 +0200

    ENUMS für Fahrzeug Typen und verfügbare Farben erstellt

---
+++ src/main/java/de/racinginternational/autolager/util/AutoType.java
@@ -0,0 +1,8 @@
+package de.racinginternational.autolager.util;
+
+public enum AutoType {
+	CABRIO,
+	KOMPAKTWAGEN,
+	KOMBI,
+	SUV	
+}
---
+++ src/main/java/de/racinginternational/autolager/util/Color.java
@@ -0,0 +1,10 @@
+package de.racinginternational.autolager.util;
+
+public enum Color {
+	RED,
+	GREEN,
+	YELLOW,
+	BLUE,
+	BLACK,
+	SILVER
+}
