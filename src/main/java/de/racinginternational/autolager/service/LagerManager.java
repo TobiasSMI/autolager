cf19a540b57e9d9bd883f9e8c9bc4931890c2b5a
Smialkowski, Tobias <tobias.smialkowski@comline.de>
2021-04-14 15:13:58 +0200

    LagerManager um autoLagerNummer ergänzt. Methode zum erhöhen und verringern des Lagerbestandes sowie auslesen des Bestandes ergänzt

--- src/main/java/de/racinginternational/autolager/service/LagerManager.java
+++ src/main/java/de/racinginternational/autolager/service/LagerManager.java
@@ -3,24 +3,37 @@ package de.racinginternational.autolager.service;
 import java.util.ArrayList;
 
 import de.racinginternational.autolager.auto.Auto;
-import de.racinginternational.autolager.auto.BMW;
-import de.racinginternational.autolager.auto.Ferrari;
-import de.racinginternational.autolager.auto.Porsche;
-import de.racinginternational.autolager.auto.Volkswagen;
 
 public class LagerManager {
-
+	private static int autoLagerNummer;
 	ArrayList<Auto> autoListe = new ArrayList<Auto>();	
 
 	public void add(Auto auto) {		
-		
+		autoListe.add(auto);
+		LagerManager.increaseAutoLagerNummer();
+	}
+	
+	public void remove (Auto auto) {
+		autoListe.remove(autoLagerNummer-1);
+		LagerManager.decreaseAutoLagerNummer();
 	}
 	
 	public void testFahren() {
 		for (Auto auto : autoListe) {
 			auto.fahren();
-		}
-		
+		}		
+	}
+
+	public int getAutoLagerNummer() {
+		return autoLagerNummer;
+	}
+
+	public static void increaseAutoLagerNummer() {
+		autoLagerNummer++;
+	}
+	
+	public static void decreaseAutoLagerNummer() {
+		autoLagerNummer--;
 	}
 	
 }
