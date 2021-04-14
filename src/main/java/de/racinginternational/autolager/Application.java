52e958481294d2244f823a8bf002f4d0dd66fada
Smialkowski, Tobias <tobias.smialkowski@comline.de>
2021-04-14 16:05:33 +0200

    4 Autotypen je 3x hinzugefügt

--- src/main/java/de/racinginternational/autolager/Application.java
+++ src/main/java/de/racinginternational/autolager/Application.java
@@ -18,7 +18,7 @@ public class Application {
 		try {
 			run.mainApplication();
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
 		} finally {
 			System.out.println("Programm beendet");
 		}
@@ -26,17 +26,35 @@ public class Application {
 	}
 	public void mainApplication() throws Exception {
 		Auto bmwHeinrichs = new BMW(30_000, Color.GREEN, AutoType.KOMBI);
+		Auto ferrariHeinrichs = new Ferrari(600_000, Color.BLACK, AutoType.CABRIO);
+		Auto porscheHeinrichs = new Porsche(100_000, Color.SILVER, AutoType.SUV);
+		Auto vwHeinrichs = new Volkswagen(20_000, Color.SILVER, AutoType.KOMPAKTWAGEN);
+		
+		Auto bmwKurdas = new BMW(40_000, Color.GREEN, AutoType.KOMBI);
 		Auto ferrariKurdas = new Ferrari(500_000, Color.BLACK, AutoType.CABRIO);
 		Auto porscheKurdas = new Porsche(50_000, Color.SILVER, AutoType.SUV);
 		Auto vwKurdas = new Volkswagen(20_000, Color.SILVER, AutoType.KOMPAKTWAGEN);
+
+		Auto bmwIsik = new BMW(100_000, Color.GREEN, AutoType.KOMBI);
+		Auto ferrariIsik = new Ferrari(700_000, Color.BLACK, AutoType.CABRIO);
+		Auto porscheIsik = new Porsche(90_000, Color.SILVER, AutoType.SUV);
+		Auto vwIsik = new Volkswagen(20_000, Color.SILVER, AutoType.KOMPAKTWAGEN);
 		
 		LagerManager lagerManager = new LagerManager();						
 		
 		lagerManager.add(bmwHeinrichs);
+		lagerManager.add(ferrariHeinrichs);
+		lagerManager.add(porscheHeinrichs);
+		lagerManager.add(vwHeinrichs);
+		lagerManager.add(bmwKurdas);
 		lagerManager.add(ferrariKurdas);
 		lagerManager.add(porscheKurdas);
-		lagerManager.add(vwKurdas); 		
-		
+		lagerManager.add(vwKurdas);
+		lagerManager.add(bmwIsik);
+		lagerManager.add(ferrariIsik);
+		lagerManager.add(porscheIsik);
+		lagerManager.add(vwIsik);
+				
 		lagerManager.testFahren();	
 		
 		// Wie viele Autos sind im Lager?
@@ -44,6 +62,8 @@ public class Application {
 		
 		// Entferne Ferrari Kurdas
 		lagerManager.remove(ferrariKurdas);
+		lagerManager.remove(ferrariKurdas);
+		lagerManager.remove(ferrariKurdas);		
 		
 		// Wie viele Autos sind im Lager?
 		System.out.println(lagerManager.getAutoLagerNummer());			
